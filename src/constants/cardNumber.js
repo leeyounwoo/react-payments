4a2fd15c9bd6aa12e2d97bf26cb597247fe20657
Leeyounwoo <yunwoo.lee@cj.net>
2024-03-25 13:42:10 +0900

    안 쓰는 상수 선언 없애기

--- src/constants/cardNumber.js
+++ src/constants/cardNumber.js
@@ -2,4 +2,3 @@ export const FIRST_NUMBER = "firstNumber";
 export const SECOND_NUMBER = "secondNumber";
 export const THIRD_NUMBER = "thirdNumber";
 export const FOURTH_NUMBER = "fourthNumber";
-export const CARD_LIST = "cardList";
